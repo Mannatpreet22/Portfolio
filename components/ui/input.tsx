8c7d5640eb404713537395072ebeef4c1ff811c4
Mannat <khurana.mannat22@gmail.com>
2025-01-23 20:53:45 -0500

    Made major changes for deployment

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -6,8 +6,8 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 import { useMotionTemplate, useMotionValue, motion } from "framer-motion";
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+// testing here
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
